1c4c666f939cb100f33cd228ebd9ad3a7a979fcd
Rafael Silva <ainbot6@gmail.com>
2024-04-08 22:16:38 -0300

    feat: add repositories

---
+++ src/main/java/myapp/authenticateAPI/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package myapp.authenticateAPI.repository;
+
+import myapp.authenticateAPI.domain.entities.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String> {
+    UserDetails findByEmail(String email);
+}
