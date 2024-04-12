c7dc966d8f1cb7d798208755e1a8154464a8262c
Rafael Silva <ainbot6@gmail.com>
2024-04-12 16:28:30 -0300

    feat: add new entities

---
+++ src/main/java/myapp/authenticateAPI/domain/entities/Tag.java
@@ -0,0 +1,38 @@
+package myapp.authenticateAPI.domain.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Document
+public class Tag implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    private String id;
+    private String name;
+    private String slug;
+
+    @DBRef(lazy = true)
+    private List<Post> posts = new ArrayList<>();
+
+    public Tag(String name, String slug){
+        this.name = name;
+        this.slug = slug;
+    }
+}
