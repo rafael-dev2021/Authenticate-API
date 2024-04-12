cd83cef29cf49fe92c2266572a61c937186fd36c
Rafael Silva <ainbot6@gmail.com>
2024-04-12 20:40:53 -0300

    up

--- src/main/java/myapp/authenticateAPI/repository/UserRepository.java
+++ src/main/java/myapp/authenticateAPI/repository/UserRepository.java
@@ -5,7 +5,12 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface UserRepository extends MongoRepository<User, String> {
     UserDetails findByEmail(String email);
+    List<User> findByIsActiveTrue();
+
+    List<User> findByIsActiveFalse();
 }
